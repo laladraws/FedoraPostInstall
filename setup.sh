18986ae17d5f5302b808782f0bf9ac4a5085d3b1
Laura <79152418+laladraws@users.noreply.github.com>
2026-03-29 14:23:27 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,10 +8,12 @@ fi
 
 #Add repositories
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf install -y mesa-va-drivers mesa-vdpau-drivers
+dnf install -y intel-media-driver
 
 #gnome
-
 dnf install \
 gdm \
 gnome-shell \
@@ -24,10 +26,29 @@ fastfetch \
 gnome-tweaks \
 rocm-hip
 
+#virtualization
+dnf install @virtualization
+systemctl enable --now libvirtd
+systemctl enable --now virtnetworkd.service
+usermod -aG libvirt $(whoami)
+
 #flatpaks
 flatpak install -y bazzar
+flatpak install flathub io.mango3d.LycheeSlicer -y
+flatpak install flathub com.vivaldi.Vivaldi -y
+flatpak install flathub com.rtosta.zapzap -y
+flatpak install flathub com.valvesoftware.Steam -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub com.visualstudio.code -y
+
+#extras
+curl -s https://ohmyposh.dev/install.sh | bash -s
+oh-my-posh font install FiraCode
 
 #remover
 dnf remove gnome-tour
+
+#file movement
 
 
