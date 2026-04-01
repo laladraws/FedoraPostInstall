1afe0b8cb219b13ae377f9b0e275064bfe26ed81
Laura <79152418+laladraws@users.noreply.github.com>
2026-04-01 12:03:56 -0300

    Simplify Tela icon theme installation command
    Removed unnecessary sudo command for icon theme installation.

--- postsetup.sh
+++ postsetup.sh
@@ -9,6 +9,6 @@ cp -r .bashrc /home/laura/
 sudo cp -r extensions/* /usr/share/gnome-shell/extensions
 cp -r wallpapers /home/laura/Imágenes
 
-#icons
- sudo -u "$ACTUAL_USER" fc-cache -fv [[ ! -d /tmp/Tela-icon-theme ]] && git clone https://github.com/vinceliuice/Tela-icon-theme.git /tmp/Tela-icon-theme && bash /tmp/Tela-icon-theme/install.sh -a && rm -rf /tmp/Tela-icon-theme
+#icong
+git clone https://github.com/vinceliuice/Tela-icon-theme.git /tmp/Tela-icon-theme && bash /tmp/Tela-icon-theme/install.sh -a && rm -rf /tmp/Tela-icon-theme
   
