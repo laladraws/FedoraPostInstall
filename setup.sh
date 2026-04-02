72883e47fc22359d34956eb34bb14005e42ffeee
laladraws <lauravonwulff@gmail.com>
2026-04-02 20:12:38 -0300

    added bridge connection to network

--- setup.sh
+++ setup.sh
@@ -50,4 +50,9 @@ usermod -aG libvirt "$SUDO_USER"
 systemctl enable gdm.service
 systemctl set-default graphical.target
 
+#bridge network for QEMU/KVM
 
+nmcli connection add type bridge ifname br0 con-name br0
+nmcli connection add type ethernet ifname enp6s0 con-name br0-slave master br0
+nmcli connection modify br0 ipv4.method auto ipv6.method auto
+nmcli connection up br0
